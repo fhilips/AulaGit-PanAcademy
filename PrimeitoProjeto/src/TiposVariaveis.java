4243b0e13b056d49d17f08c70b831d127700bbb5
fhilips <fhilips.souza@gmail.com>
2021-10-09 14:01:00 -0300

    Primeiro Projeto

---
+++ PrimeitoProjeto/src/TiposVariaveis.java
@@ -0,0 +1,23 @@
+
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		byte tipoByte = 123;
+		short tipoShort = 54;
+		char tipoChar = 'A';
+		float tipoFloat = 2.1f;
+		double tipoDouble = 1.52d;
+		int tipoInt = 1145677;
+		long tipoLong = 123468795;
+		boolean tipoBooleano = true;
+		
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " + tipoFloat);
+		System.out.println("Valor do tipoDouble = " + tipoDouble);
+		System.out.println("Valor do tipoInt = " + tipoInt);
+		System.out.println("Valor do tipoLong = " + tipoLong);
+		System.out.println("Valor do tipoBooleano = " + tipoBooleano);
+	}
+
+}
